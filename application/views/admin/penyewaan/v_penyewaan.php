61985ec874696e61d9c914926516863b1f265d89
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-12-28 07:20:00 +0700

    input data penyewaan ok

--- application/views/admin/penyewaan/v_penyewaan.php
+++ application/views/admin/penyewaan/v_penyewaan.php
@@ -32,7 +32,7 @@
                             <th class="text-center text-white">Status</th>
 
                             <!-- <th class="text-center text-white">Foto</th> -->
-                            <th class="text-center text-white" width="300px"></th>
+                            <!-- <th class="text-center text-white" width="300px"></th> -->
 
                         </tr>
                     </thead>
@@ -59,8 +59,8 @@
                                 </td>
                                 <td class="float-right">
                                     <!-- <a href="" class="btn btn-primary btn-sm mb-1" data-toggle="modal" data-target="#datadetail<?php echo $a->kd_penyewaan ?>"> <i class="fa fa-info mr-2"></i> Detail</a> -->
-                                    <a href="" class="btn btn-info btn-sm mb-1" data-toggle="modal" data-target="#editdata<?php echo $a->kd_penyewaan ?>"> <i class="fa fa-edit mr-2"></i> Edit</a>
-                                    <a href="" class="btn btn-danger btn-sm mb-1" data-toggle="modal" data-target="#hapusdata<?php echo $a->kd_penyewaan ?>"> <i class="fa fa-trash mr-2"></i> Hapus</a>
+                                    <!-- <a href="" class="btn btn-info btn-sm mb-1" data-toggle="modal" data-target="#editdata<?php echo $a->kd_penyewaan ?>"> <i class="fa fa-edit mr-2"></i> Edit</a> -->
+                                    <!-- <a href="" class="btn btn-danger btn-sm mb-1" data-toggle="modal" data-target="#hapusdata<?php echo $a->kd_penyewaan ?>"> <i class="fa fa-trash mr-2"></i> Hapus</a> -->
                                 </td>
                             </tr>
                         <?php endforeach ?>
@@ -166,128 +166,3 @@
         </div>
     </div>
 </div>
-
-<!-- modal detail -->
-<?php foreach ($penyewaan as $a) : ?>
-    <div class="modal fade" id="datadetail<?php echo $a->kd_penyewaan ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-aqua">
-                    <h5 class="modal-title" id="exampleModalLabel"> <i class="fa fa fa-futbol-o mr-2"></i> Detail Data penyewaan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-borderless">
-                        <tr>
-                            <th>Kode penyewaan</th>
-                            <td><?php echo $a->kd_penyewaan ?></td>
-                        </tr>
-                        <tr>
-                            <th>Nama penyewaan</th>
-                            <td><?php echo $a->nama_penyewaan ?></td>
-                        </tr>
-
-
-
-                        <!-- <tr>
-                            <th>Foto penyewaan</th>
-                            <td>
-                                <img src="<?php echo base_url('assets/toko/images/penyewaan/') . $a->foto_penyewaan ?>" alt="">
-                            </td>
-                        </tr> -->
-
-                    </table>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                    <!-- <button type="submit" class="btn btn-primary">Simpan</button> -->
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-<!-- akhir detail -->
-<!-- modal detail -->
-<?php foreach ($penyewaan as $a) : ?>
-
-
-    <div class="modal fade" id="hapusdata<?php echo $a->kd_penyewaan ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-sm" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-danger text-white">
-                    <h5 class="modal-title" id="exampleModalLabel"> <i class="fa fa fa-futbol-o mr-2"></i> Hapus Data</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?php echo base_url('admin/master/penyewaan/hapuspenyewaan') ?>" method="post">
-                        <div class="form-group">
-                            Apakah Anda Yakin akan menghapus data ini ?
-                            <!-- <label for="">Nama</label>
-                  <input name="nama_penyewaan" type="text" class="form-control" value="<?php echo $a->nama_penyewaan ?>" required> -->
-                            <input name="kd_penyewaan" type="hidden" class="form-control" value="<?php echo $a->kd_penyewaan ?>" required>
-                        </div>
-
-
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
-                    <button type="submit" class="btn btn-danger">Ya</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-<!-- akhir detail -->
-
-<!-- Modal -->
-<?php foreach ($penyewaan as $a) : ?>
-
-
-    <div class="modal fade" id="editdata<?php echo $a->kd_penyewaan ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-aqua">
-                    <h5 class="modal-title" id="exampleModalLabel"> <i class="fa fa fa-futbol-o mr-2"></i> Form Edit Data</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?php echo base_url('admin/master/penyewaan/aksieditpenyewaan') ?>" method="post">
-                        <div class="form-group">
-                            <label for="">Nama penyewaan</label>
-                            <input name="nama_penyewaan" type="text" class="form-control" value="<?php echo $a->nama_penyewaan ?>" required>
-                            <input name="kd_penyewaan" type="hidden" class="form-control" value="<?php echo $a->kd_penyewaan ?>" required>
-                        </div>
-
-
-
-
-
-                        <!-- <div class="form-group">
-                            <label for="">Foto penyewaan</label>
-                            <img class="form-control img-fluid mb-1" src="<?php echo base_url('assets/toko/images/penyewaan/') . $a->foto_penyewaan ?>" alt="">
-                            <input name="foto_penyewaan" type="file" class="form-control" value="<?php echo $a->foto_penyewaan ?>">
-                        </div> -->
-
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fa fa-close mr-2"></i>Close</button>
-                    <button type="submit" class="btn btn-primary"> <i class="fa fa-save mr-2"></i>Simpan</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-<?php endforeach; ?>
